f69021dc9c22971ea22ea155658b3894cb72f61e
lidiyazakharova <lidijaromanowa@gmail.com>
2024-02-18 10:58:27 +0300

    v1.0

--- Tracker/TrackersScreenViewController.swift
+++ Tracker/TrackersScreenViewController.swift
@@ -1,16 +1,16 @@
 import UIKit
 
-final class TrackersScreenViewController: UIViewController {
+final class MainScreenViewController: UIViewController {
     
     //MARK: - Private Properties
-    private lazy var trackersScreenImage: UIImageView = {
-        let trackersScreenImage = UIImageView()
-        trackersScreenImage.image = UIImage(named: "EmptyTrackerIcon")
-        trackersScreenImage.contentMode = .scaleToFill
-        trackersScreenImage.translatesAutoresizingMaskIntoConstraints = false
-        trackersScreenImage.heightAnchor.constraint(equalToConstant: 80).isActive = true
-        trackersScreenImage.widthAnchor.constraint(equalToConstant: 80).isActive = true
-        return trackersScreenImage
+    private lazy var mainScreenImage: UIImageView = {
+        let mainScreenImage = UIImageView()
+        mainScreenImage.image = UIImage(named: "EmptyTrackerIcon")
+        mainScreenImage.contentMode = .scaleToFill
+        mainScreenImage.translatesAutoresizingMaskIntoConstraints = false
+        mainScreenImage.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        mainScreenImage.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        return mainScreenImage
     } ()
     
     private lazy var questionLabel: UILabel = {
@@ -35,16 +35,16 @@ final class TrackersScreenViewController: UIViewController {
     
     //MARK: - Private Functions
     private func setImage(){
-        view.addSubview(trackersScreenImage)
+        view.addSubview(mainScreenImage)
         NSLayoutConstraint.activate([
-            trackersScreenImage.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            trackersScreenImage.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            mainScreenImage.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            mainScreenImage.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
     private func setQuestionLabel(){
         view.addSubview(questionLabel)
         NSLayoutConstraint.activate([
-            questionLabel.topAnchor.constraint(equalTo: trackersScreenImage.bottomAnchor, constant: 8),
+            questionLabel.topAnchor.constraint(equalTo: mainScreenImage.bottomAnchor, constant: 8),
             questionLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             questionLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
         ])
